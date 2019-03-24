ef052e82b6f1da5b2f8ff8a7437918496e59a0ad
Boris <boris_bfh@mail.ru>
2019-03-25 00:39:28 +0300

    Some top words functions added

--- HHWorkStats/settings.py
+++ HHWorkStats/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%z!cwyl12+glat0c1dvj-=m_nt1z2vviaujo6&@!f%5c!s7_+k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','kbn-pc']
+ALLOWED_HOSTS = ['localhost', 'kbn-pc']
 
 
 # Application definition
@@ -125,3 +125,32 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = 'static'
+
+if DEBUG:
+   INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+   MIDDLEWARE += (
+       'debug_toolbar.middleware.DebugToolbarMiddleware',
+   )
+
+   INSTALLED_APPS += (
+       'debug_toolbar',
+   )
+
+   DEBUG_TOOLBAR_PANELS = [
+       'debug_toolbar.panels.versions.VersionsPanel',
+       'debug_toolbar.panels.timer.TimerPanel',
+       'debug_toolbar.panels.settings.SettingsPanel',
+       'debug_toolbar.panels.headers.HeadersPanel',
+       'debug_toolbar.panels.request.RequestPanel',
+       'debug_toolbar.panels.sql.SQLPanel',
+       'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+       'debug_toolbar.panels.templates.TemplatesPanel',
+       'debug_toolbar.panels.cache.CachePanel',
+       'debug_toolbar.panels.signals.SignalsPanel',
+       'debug_toolbar.panels.logging.LoggingPanel',
+       'debug_toolbar.panels.redirects.RedirectsPanel',
+   ]
+
+   DEBUG_TOOLBAR_CONFIG = {
+       'INTERCEPT_REDIRECTS': False,
+   }
--- workstats/views.py
+++ workstats/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from .models import Stats
+from .models import Stats, Vacancies, VacanciesDetails
 from django.core.paginator import Paginator
+import pandas as pd
+import pdb
 
 # Create your views here.
 
 class IndexView(generic.ListView):
-    template_name = 'index.html'
+    template_name = 'workstats/index.html'
     context_object_name = 'stats_list'
-    
+
     paginate_by = 25
 
-    search_fields = ['found']
+    #search_fields = ['found']
 
     def get_queryset(self):
         return Stats.objects.all()
+
+class StatsView(generic.ListView):
+    template_name = 'workstats/stats.html'
+    context_object_name = 'stats_nums'
+    
+    text = VacanciesDetails.objects.values('description')
+
+    df = pd.DataFrame(text)
+
+    output_info = df.description.str.split(expand=True).stack().value_counts()
+
+    def get_queryset(self):
+        return self.output_info[:50]
